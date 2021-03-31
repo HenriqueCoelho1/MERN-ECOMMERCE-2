430ff4c428b0f705ddf04029476391c7003ff17d
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-03-31 02:30:01 -0300

    Email Link To SignIn

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
 import { Col, Row, Container, Form, Button } from 'react-bootstrap'
-
+import { auth } from '../firebase'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 
 const RegisterScreen = () => {
     const { Control } = Form
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
+        e.preventDefault()//this stop the browser from reload when the button action is called
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true
+        }
 
+        await auth.sendSignInLinkToEmail(email, config)
+        toast.success(`Email is sent to ${email}. Click the link to complete your registration`)
+
+
+        //save your email in local storage
+        window.localStorage.setItem('emailForRegistration', email)
+        //clear state
+        setEmail('')
     }
 
 
@@ -18,6 +33,7 @@ const RegisterScreen = () => {
             <Row>
                 <Col md={{ span: 6, offset: 3 }}>
                     <h4>Register</h4>
+                    <ToastContainer />
 
                     <Form onSubmit={handleSubmit}>
                         <Control
