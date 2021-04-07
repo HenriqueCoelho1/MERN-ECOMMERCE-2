f009c2e1ce53532459bab228466beda3a979a131
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-07 17:42:11 -0300

    Controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+export const createOrUpdateUser = (req, res) => {
+    res.json({
+        data: 'You hit the endpoint'
+    })
+}
+
+export const home = (req, res) => {
+    res.json({
+        data: 'Hello user route'
+    })
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,17 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 //controllers
+import { home, createOrUpdateUser } from '../controllers/userController.js'
 
-router.get('/', (req, res) => {
-    res.json({
-        data: 'Hello user route'
-    })
-})
-router.get('/create-or-update-user', (req, res) => {
-    res.json({
-        data: 'You hit the endpoint'
-    })
-})
+router.get('/', home)
+router.get('/create-or-update-user', createOrUpdateUser)
 
 
 
