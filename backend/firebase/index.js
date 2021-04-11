030f0eff6e193c85a523e3a5114a56bc5f191cc1
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-04-10 22:28:06 -0300

    Fixing firebase data

--- backend/firebase/index.js
+++ backend/firebase/index.js
@@ -3,8 +3,7 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ecommerce-225c8.firebaseio.com",
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
