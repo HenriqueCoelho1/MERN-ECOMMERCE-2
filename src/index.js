51207891e9abf9c1d1ae81e5a9cfa44f321a4615
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-07-17 22:49:11 -0300

    New Reborn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from './store'
+import './index.css';
 import App from './App';
-import './index.css'
 import reportWebVitals from './reportWebVitals';
-import 'antd/dist/antd.css'
-
 
 ReactDOM.render(
-  //<React.StrictMode>
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-  //</React.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
