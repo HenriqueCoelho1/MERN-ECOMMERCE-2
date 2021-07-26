7416de70eded65bb53fb3dedf799126aec5494f8
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-07-26 20:46:42 -0300

    user is logged or not logged

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -10,7 +10,7 @@ import {
 } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
 import firebase from 'firebase'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { LOGOUT } from '../../actions/types'
 import { useHistory } from 'react-router-dom'
 
@@ -21,6 +21,7 @@ const Header = () => {
 
     const [home, setHome] = useState('')
     const dispatch = useDispatch()
+    const { user } = useSelector((state) => ({ ...state }))
     const history = useHistory()
 
     const handleClick = (e) => {
@@ -36,27 +37,37 @@ const Header = () => {
         history.push('/login')
     }
 
+
     return (
-        <Menu onClick={handleClick} selectedKeys={[home]} mode="horizontal">
-            <Item key="home" icon={<AppstoreOutlined />}>
-                <Link to="/">Home</Link>
-            </Item>
+        <>
+            {user && <Menu onClick={handleClick} selectedKeys={[home]} mode="horizontal">
+                <Item key="home" icon={<AppstoreOutlined />}>
+                    <Link to="/">Home</Link>
+                </Item>
 
 
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username" className="mr-auto">
-                <Item key="setting:1">Register</Item>
-                <Item key="setting:2">Option 2</Item>
-                <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
+                <SubMenu key="SubMenu" icon={<SettingOutlined />} title={user.email && user.email.split('@')[0]} className="ml-auto">
+                    <Item key="setting:1">Option 1</Item>
+                    <Item key="setting:2">Option 2</Item>
+                    <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
 
-            </SubMenu>
+                </SubMenu>
+            </Menu>}
 
-            <Item key="register" icon={<UserAddOutlined />}>
-                <Link to="/register">Register</Link>
-            </Item>
-            <Item key="login" icon={<UserOutlined />}>
-                <Link to="/Login">Login</Link>
-            </Item>
-        </Menu>
+            {!user && <Menu onClick={handleClick} selectedKeys={[home]} mode="horizontal">
+                <Item key="home" icon={<AppstoreOutlined />} className="mr-auto">
+                    <Link to="/">Home</Link>
+                </Item>
+
+
+                <Item key="register" icon={<UserAddOutlined />}>
+                    <Link to="/register">Register</Link>
+                </Item>
+                <Item key="login" icon={<UserOutlined />}>
+                    <Link to="/Login">Login</Link>
+                </Item>
+            </Menu>}
+        </>
     )
 
 }
