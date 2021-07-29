904a48a4bfbf34b1bd49b33c2e8aa8dd5ae117eb
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-07-29 00:59:37 -0300

    Middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,29 @@ const express = require('express')
 
 const router = express.Router()
 
+const { authCheck } = require('../middleware/auth')
 const { createOrUpdateUser } = require('../controllers/auth')
 
-router.get("/create-or-update-user", createOrUpdateUser)
+router.post("/create-or-update-user", authCheck, createOrUpdateUser)
+
 
 router.get('/hello', (req, res) => {
     res.json({
         data: "Hey you hit node API endpoint"
     })
 })
+
+
+// //test
+// const checking = (req, res, next) => {
+//     console.log('Hello World!')
+//     next()
+// }
+
+// router.get('/test', checking, (req, res) => {
+//     res.json({
+//         data: 'Hello World!'
+//     })
+// })
 
 module.exports = router
