09596687d6fe4531fc877d393e6a6bc1f7f1ea35
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-23 18:32:34 -0300

    Fetch all products

--- client/src/pages/admin/Dashboard.js
+++ client/src/pages/admin/Dashboard.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AdminNav from '../../components/nav/AdminNav'
+import { getProductsByCount } from '../../functions/product'
 
 const Dashboard = () => {
+
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        loadAllProducts()
+
+    }, [])
+
+    const loadAllProducts = () => {
+        setLoading(true)
+        getProductsByCount(100)
+            .then(res => {
+                setProducts(res.data)
+                setLoading(false)
+            }).catch(err => {
+                setLoading(false)
+                console.log(err)
+            })
+    }
+
     return (
 
         <div className="container-fluid">
@@ -9,7 +31,8 @@ const Dashboard = () => {
                 <div className="col-md-2">
                     <AdminNav />
                 </div>
-                <div className="col">Admin Dashboard</div>
+                {loading ? (<h4 className="text-danger">Loading...</h4>) : (<h4>All Products</h4>)}
+                <div className="col">{JSON.stringify(products)}</div>
             </div>
         </div>
 
