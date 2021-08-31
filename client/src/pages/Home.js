7f50aacf8cf8565a1ed9b9f6fdab2d439a78c8f1
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-31 18:05:26 -0300

    Product Card

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getProductsByCount } from '../functions/product'
+import ProductCard from '../components/cards/ProductCard'
 
 const Home = () => {
     const [products, setProducts] = useState([])
@@ -11,17 +12,31 @@ const Home = () => {
     }, [])
 
     const loadAllProducts = () => {
+        setLoading(true)
         getProductsByCount(3)
             .then(res => {
+                setLoading(false)
                 setProducts(res.data)
             })
 
     }
     return (
-        <div>
-            <p> Home </p>
-            {JSON.stringify(products)}
-        </div>
+        <>
+            <div className="jumbotron">
+                {loading ? (<h4 className="text-danger">Loading...</h4>) : (<h4>All Products</h4>)}
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    {products.map((product) => (
+                        <div key={product._id} className="col-md-4">
+                            <ProductCard product={product} />
+                        </div>
+
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
 
