cc8e953d787694f421bb9b5dfd1866ae2b2d3046
HenriqueCoelho1 <alleson_henrique@hotmail.com>
2021-08-31 18:36:04 -0300

    And design card

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Card } from 'antd'
+import { EyeOutlined, ShoppingCartOutlined } from '@ant-design/icons'
+import defaultImage from '../../images/default.png'
+import { Link } from 'react-router-dom'
 
 const ProductCard = ({ product }) => {
+    const { images, title, description, slug } = product
+    const { Meta } = Card
     return (
-        <div>
-            {product.title}
-        </div>
+        <Card cover={
+            <img src={images && images.length ? images[0].url : defaultImage}
+                style={{ height: "150px", objectFit: "cover" }}
+                className="p-1" />
+        }
+            actions={[
+                <Link to={`/product/${slug}`}>
+                    <EyeOutlined className="text-warning" /> <br /> View Product
+                </Link>,
+                <><ShoppingCartOutlined className="text-info" /> <br /> View Product</>
+            ]}>
+
+            <Meta title={title}
+                description={`${description && description.substring(0, 40)}...`} />
+        </Card>
     )
 }
 
